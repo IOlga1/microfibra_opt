c5a311a4a6fab17f64b911a5665eaacf2395d28f
IOlga1 <ivaniushyna.olga@gmail.com>
2022-06-19 23:37:55 +0300

    add calc for user

---
+++ docs/js/calc.js
@@ -0,0 +1,44 @@
+let getActualWidth = (event) => {
+    switch (event.target.value) {
+        case '':
+            width.textContent = '0 cm';
+            break;
+        case 'classic':
+            width.textContent = '165 cm';
+            break;
+        case 'smooth':
+            width.textContent = '165 cm';
+            break;
+        case 'rubber':
+            width.textContent = '100 cm';
+            break;
+        default:
+            break;
+    }
+    getActualWeightOne();
+    getActualWeightAll();
+}
+
+let getActualWeightOne = () => {
+    weightOne.textContent = destiny.value * (parseInt(width.textContent)/100);
+    getActualWeightAll();
+}
+
+let getActualWeightAll = () => {
+    weightAll.textContent = footage.value * parseInt(weightOne.textContent)/1000 + ' кг';
+}
+
+    let product = document.querySelector('.select_product');
+    product.addEventListener('change', getActualWidth);
+
+    let width = document.querySelector('.width_text');
+
+    let destiny = document.querySelector('.select_destiny');
+    destiny.addEventListener('change', getActualWeightOne);
+
+    let weightOne = document.querySelector('.weight_one_text');
+
+    let footage = document.querySelector('.footage_input_text');
+    footage.addEventListener('change', getActualWeightAll);
+
+    let weightAll = document.querySelector('.weight_all_text');
